6b3e5cb491b452f417bb70e127be0c7bed508403
Taneros <fricentaurus@GMAIL.COM>
2021-08-13 20:13:06 +0300

    feat: solve for custom case with variable input

--- src/myIndex3.js
+++ src/myIndex3.js
@@ -9,34 +9,65 @@ function check(str, bracketsConfig) {
 
   // describe open brackets
   //  -> place odd lements from brackets config here
-  const OPEN_BRACKETS = ['7']
+
+  const OPEN_BRACKETS = []
+
+  const CLOSE_BRACKETS = []
+
+  const BRACKETS_PAIR = {}
+
+  for (let innerArr of bracketsConfig) {
+    // console.log('innerArr[0]', innerArr[0])
+    OPEN_BRACKETS.push(innerArr[0])
+    // console.log('innerArr[1]', innerArr[1])
+    CLOSE_BRACKETS.push(innerArr[1])
+  }
+
+  for (let i in OPEN_BRACKETS) {
+    // console.log('i', i)
+    BRACKETS_PAIR[[CLOSE_BRACKETS[i]]] = OPEN_BRACKETS[i]
+  }
+
+  // let k = ['a', 'b', 'c']
+
+  // let obj = k.reduce(function (acc, curr) {
+  //   acc[curr] = '';
+  //   return acc;
+  // }, {});
+
+
+
+  console.log('OPEN_BRACKETS', OPEN_BRACKETS)
+  console.log('CLOSE_BRACKETS', CLOSE_BRACKETS)
+  console.log('BRACKETS_PAIR', BRACKETS_PAIR)
 
   // brackets pari dictionary
-  const BRACKETS_PAIR = {
-    ['8']: '7',
-    ['9']: '8',
-  }
 
 
   let stack = []
 
   for (let char of str) {
 
-    console.log('char', char)
     let currentChar = char
+    console.log('currentChar', currentChar)
 
-    // check if OPEN_BRACKETS contain current char form str
+    // check if current char is opening char
     if (OPEN_BRACKETS.includes(currentChar)) {
+      console.log('it is opening char -> put it in stack')
+      console.log('stack', stack)
       stack.push(currentChar)
     }
-    // check if stack is not empty -> if it si not move to stack is not empty case
+    // check if stack is not empty -> if it is -> not move to stack is not empty case
     else {
       if (stack.length === 0) {
+        console.log('stack is empty and it is a closing char')
         return false
       }
 
       // stack is not empty case
       let topStackEl = stack[stack.length - 1]
+      console.log('topStackEl', topStackEl)
+      console.log('BRACKETS_PAIR[currentChar]', BRACKETS_PAIR[currentChar])
 
       if (BRACKETS_PAIR[currentChar] === topStackEl) {
         stack.pop()
@@ -46,10 +77,10 @@ function check(str, bracketsConfig) {
         return false
       }
     }
-    console.log('stack', stack)
+    console.log('stack after', stack)
   }
 
   return stack.length === 0
 }
 
-console.log(check('78787878', [['7', '8']]))
+console.log(check('787878789', [['7', '8'], ['9', '10']]))
